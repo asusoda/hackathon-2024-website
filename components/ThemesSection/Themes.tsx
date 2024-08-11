92bce99d59bec8616d1233542a50d0074fcd7da9
darshchaurasia <darshchaurasia@gmail.com>
2024-08-11 10:29:09 -0700

    added missing key prop

--- components/ThemesSection/Themes.tsx
+++ components/ThemesSection/Themes.tsx
@@ -4,6 +4,7 @@ import "../../styles/themes.css";
 import "../../styles/themes/background.css";
 import Image from "next/image";
 import Tilt from "react-parallax-tilt";
+
 const themes = [
   {
     title: "Education",
@@ -53,7 +54,7 @@ export default function Themes() {
       <h1 className="mt-36 sm:mt-56 text-center text-white text-3xl sm:text-4xl md:text-4xl xl:text-5xl font-logo">
         Themes
       </h1>
-      {/* <div className="slider">
+            {/* <div className="slider">
         <div className="slide-track-10 hover:pause mt-6 grid grid-cols-4 sm:grid-cols-4 md:grid-cols-8 justify-around items-center">
           {[...themes, ...themes].map(({ title, image }) => (
             <div className="w-[12rem] relative opacity-100 transition duration-200">
@@ -72,21 +73,23 @@ export default function Themes() {
           ))}
         </div>
       </div> */}
-      <div className="flex justify-center mt-28 ">
-        <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 md:gap-x-8 md:gap-y-8 gap-3    themeContainer1">
-          {[...themes].map(({ title, image }) => (
-            <div className="max-w-md mx-auto  md:w-64 md:h-full sm:w-44 w-40 h-full  overflow-hidden ">
+      <div className="flex justify-center mt-28">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 md:gap-x-8 md:gap-y-8 gap-3 themeContainer1">
+          {themes.map(({ title, image }) => (
+            <div
+              key={title}  // Add key prop here
+              className="max-w-md mx-auto md:w-64 md:h-full sm:w-44 w-40 h-full overflow-hidden"
+            >
               <Card
-                key={title}
                 title={title}
                 icon={
                   <Image
                     src={image}
                     alt={title}
                     width={80}
-            height={80}
-            objectFit="cover"
-                    className="md:w-[10rem] md:h-[10rem] w-[15rem] h-[rem] "
+                    height={80}
+                    style={{ objectFit: "cover" }}  // Using style instead of objectFit prop
+                    className="md:w-[10rem] md:h-[10rem] w-[15rem] h-[rem]"
                   />
                 }
               />
@@ -107,13 +110,12 @@ function Card(props: any) {
       glareEnable
       tiltAngleYInitial={0}
       glareMaxOpacity={0.1}
-      className="fix-safari-tilt relative overflow-hidden rounded-2xl glassy-effect p-px "
+      className="fix-safari-tilt relative overflow-hidden rounded-2xl glassy-effect p-px"
     >
-      <div className="relative flex md:h-64 h-48 flex-col gap-3 rounded-2xl  p-7 ">
+      <div className="relative flex md:h-64 h-48 flex-col gap-3 rounded-2xl p-7">
         <div className="flex items-center justify-center">{icon}</div>
-
         <div>
-          <h4 className="md:text-xl  text-md text-center font-semibold text-white dark:text-white">
+          <h4 className="md:text-xl text-md text-center font-semibold text-white dark:text-white">
             {title}
           </h4>
         </div>
