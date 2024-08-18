cae714ea7939024d4c0b92e043d958b411ea0a67
darshchaurasia <darshchaurasia@gmail.com>
2024-08-17 17:31:30 -0700

    Rough flow timeline update

--- components/Timeline/schedule.js
+++ components/Timeline/schedule.js
@@ -1,58 +1,58 @@
 const items = [
   {
     key: 0,
-    title: "15th Feb 5:3O PM",
-    cardTitle: "Workshops Starts",
+    title: "30th Aug 5:30 PM",
+    cardTitle: "Registration Starts",
     cardDetailedText:
-      "Starting off with guidance sessions...",
+      "Starting off with online registration...",
   },
   {
     key: 1,
-    title: "20th Feb 10:OO PM",
+    title: "25th Sep 10:00 PM",
     cardTitle: "Registration Ends",
     cardDetailedText: "We'll review your application and will let you know.",
   },
   {
     key: 2,
-    title: "24th Feb 8:3O AM",
+    title: "5th Oct 8:00 AM",
     cardTitle: "Check-in starts",
     cardDetailedText: "Check-in and get ready for the event.",
   },
   {
     key: 3,
-    title: "24th Feb 9:00 AM",
+    title: "5th Oct 9:30 AM",
     cardTitle: "Hackathon Opening Ceremony",
     cardDetailedText:
-      "Let's kickoff DUHacks 3.0 with us !",
+      "Let's kickoff HackSODA 24 with us !",
   },
   {
     key: 4,
-    title: "24th Feb 11:0O AM",
+    title: "5th Oct 12:00 PM",
     cardTitle: "Hacking Period Starts",
-    cardDetailedText: "Join with over 2500+ innovative minds and start building",
+    cardDetailedText: "Join with over 200+ innovative minds and start building",
   },
   {
     key: 5,
-    title: "24th Feb 8:0O PM",
+    title: "5th Oct 8:00 PM",
     cardTitle: "Initial Submission",
     cardDetailedText: "Initial Submission on github",
   },
   {
     key: 6,
-    title: "25th Feb 10:30 AM",
+    title: "6th Oct 10:30 AM",
     cardTitle: "Session Time!",
     cardDetailedText:"Get ready for some amazing sessions.",
   },
   {
     key: 7,
-    title: "25th Feb 8:OO PM",
+    title: "6th Oct 4:00 PM",
     cardTitle: "Final Submission Starts",
-    cardDetailedText: "Submit your project on duhacks devfolio dashboard",
+    cardDetailedText: "Submit your project.",
   },
 
   {
     key: 8,
-    title: "25th Feb 10:00 PM",
+    title: "6th Oct 4:30 PM",
     cardTitle: "Final Submission Ends",
     cardDetailedText: "We'll review your project and will let you know.",
   },
