08566f9cf74c436ba9658c79248d7db8c02269ce
darshchaurasia <darshchaurasia@gmail.com>
2024-08-17 18:47:42 -0700

    fixed footer section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,6 @@
 "use client";
-import React, { useState } from "react";
-import { SocialIcon } from "react-social-icons";
-
-import {
-  FaDiscord,
-  FaInstagram,
-  FaLinkedinIn,
-  FaMailBulk,
-  FaTwitter,
-  FaYoutube,
-} from "react-icons/fa";
-
+import React from "react";
+import { FaDiscord, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
@@ -25,52 +15,51 @@ const Footer = () => {
 
   return (
     <footer className="mt-24 pt-5 lg:pt-8 pb-2 lg:pb-8 bg-black bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20">
-      <div className="flex flex-col">
-        <div className="lg:hidden flex justify-center mb-7 font-bold"/>
-      
+      <div className="flex flex-col items-center">
+        <div className="lg:hidden flex justify-center mb-7 font-bold" />
+        
+        {/* Social media logos */}
         <div className="flex justify-center text-sm lg:text-3xl space-x-4 mb-9">
-          <a href="https://discord.gg/the-software-developers-association-762811961238618122">
+          <a href={SOCIALS.discord}>
             <span className="sr-only">Discord</span>
-           <FaDiscord/>
+            <FaDiscord />
           </a>
-          <div className=" border-r-2 transform rotate-12 " />
-          <a href="https://x.com/asu_soda">
+          <div className="border-r-2 transform rotate-12" />
+          <a href={SOCIALS.twitter}>
             <span className="sr-only">Twitter</span>
-            <FaXTwitter/>
-
+            <FaXTwitter />
           </a>
-          <div className=" border-r-2 transform rotate-12" />
-
-          <a href="https://www.linkedin.com/company/thesoda/">
-            <span className="sr-only ">LinkedIn</span>
-            <FaLinkedinIn/>
-
+          <div className="border-r-2 transform rotate-12" />
+          <a href={SOCIALS.linkedin}>
+            <span className="sr-only">LinkedIn</span>
+            <FaLinkedinIn />
           </a>
-          <div className=" border-r-2 transform rotate-12" />
-
+          <div className="border-r-2 transform rotate-12" />
           <a href="https://www.youtube.com/@ASUSoDAYoutube">
             <span className="sr-only">YouTube</span>
-            <FaYoutube/>
+            <FaYoutube />
           </a>
-          <div className=" border-r-2 transform rotate-12" />
-
-          <a href="https://instagram.com/soda.asu">
+          <div className="border-r-2 transform rotate-12" />
+          <a href={SOCIALS.instagram}>
             <span className="sr-only">Instagram</span>
-            <FaInstagram/>
+            <FaInstagram />
           </a>
         </div>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-8 lg:gap-y-0 xl:gap-x-36 items-center justify-around text-xs xl:text-sm">
-            <div className="lg:grid grid-cols-3 text-center hidden">
-            <a  href="mailto:asu@thesoda.io">
+
+        {/* Footer content hidden on mobile */}
+        <div className="hidden lg:flex w-full justify-center">
+          <div className="flex flex-col items-center text-center space-y-4">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-y-4 lg:gap-y-0 xl:gap-x-36 text-xs xl:text-sm">
+              <a href={SOCIALS.email}>
                 Contact Us
-            </a>
-            <div className="mx-5 lg:mx-0 text-center">
-              Copyrights © 2024. All rights reserved by The Software Developers Association
+              </a>
+              <div className="mx-5 lg:mx-0">
+                Copyrights © 2024. All rights reserved by The Software Developers Association
+              </div>
+              <a href={SOCIALS.discord} target="_blank" rel="noreferrer">
+                Join Discord
+              </a>
             </div>
-             <a href="https://discord.gg/the-software-developers-association-762811961238618122" target="_blank" rel="noreferrer">Join Discord</a>
-            </div>
-            
           </div>
         </div>
       </div>
