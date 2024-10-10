3cb046924824374701e1c67063a243f5f3c0d33a
rtwoo <rtwoo@asu.edu>
2024-10-10 01:08:56 -0700

    Remove basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/hackathon-2024-website',
-  assetPrefix: '/hackathon-2024-website/',
-  images: {
-    domains: ['res.cloudinary.com', 't4.ftcdn.net', 'raw.githubusercontent.com'],
-  }
+    images: {
+        domains: [ 'res.cloudinary.com','t4.ftcdn.net','raw.githubusercontent.com'],
+    }
 }
 
 module.exports = nextConfig
