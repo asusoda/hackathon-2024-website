1bede205692e5f37532235d9ccae23e42da44793
rtwoo <rtwoo@asu.edu>
2024-10-10 00:31:58 -0700

    Update next.config.js to include basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [ 'res.cloudinary.com','t4.ftcdn.net','raw.githubusercontent.com'],
-    }
+  basePath: '/hackathon-2024-website',
+  assetPrefix: '/hackathon-2024-website/',
+  images: {
+    domains: ['res.cloudinary.com', 't4.ftcdn.net', 'raw.githubusercontent.com'],
+  }
 }
 
 module.exports = nextConfig
