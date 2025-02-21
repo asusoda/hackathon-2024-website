e6463b0f874f9bf91328f58407494c8dae7a0cbf
code-wolf-byte <hackerstark37@gmail.com>
2025-02-21 16:08:38 -0700

    Fixed nextjs export config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: [ 'res.cloudinary.com','t4.ftcdn.net','raw.githubusercontent.com'],
-    }
-}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    domains: ['res.cloudinary.com', 't4.ftcdn.net', 'raw.githubusercontent.com'],
+  },
+  assetPrefix: isProd ? '/code-challenge-2025/' : '', // Important for GitHub Pages
+  basePath: isProd ? '/code-challenge-2025' : '', // Ensure correct routing
+  trailingSlash: true, // Ensure proper asset paths
+};
+
+module.exports = nextConfig;
